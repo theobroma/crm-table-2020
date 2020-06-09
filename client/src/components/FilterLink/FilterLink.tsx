4e46d896317dfa3427f210fa4d28834efbe266fd
Alexandr <theobroma222@gmail.com>
2020-06-09 04:07:29 +0300

    activeFiltersCount

--- client/src/components/FilterLink/FilterLink.tsx
+++ client/src/components/FilterLink/FilterLink.tsx
@@ -2,61 +2,44 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import cx from 'classnames';
 import { actions } from '../../store/filter/actions';
-import { filterSelector } from '../../store/filter/selectors';
-import { FilterItemType } from '../../store/filter/types';
+import {
+  filterSelector,
+  activeFiltersSelector,
+} from '../../store/filter/selectors';
+import { FilterItemType, FilterType } from '../../store/filter/types';
 import { Styled } from './FilterLink.styled';
 
 interface Props {
-  type?: any;
-  toggleFilter?: any;
-  filter?: any;
-  filters?: any;
-  activeFiltersCount?: any;
+  type: FilterType;
 }
 
-const FilterLink: React.FC<Props> = React.memo((props) => {
+const FilterLink: React.FC<Props> = React.memo(({ children, type }) => {
   const dispatch = useDispatch();
   const filterData = useSelector(filterSelector).data;
+  const activeFiltersCount = useSelector(activeFiltersSelector).length;
 
-  const filter2 = filterData.filter((f: FilterItemType) => {
-    console.log('f:', f);
-    console.log('type:', props.type);
-    return f.name === props.type;
+  const currentFilter = filterData.filter((f: FilterItemType) => {
+    return f.name === type;
   })[0];
 
-  // console.log(filter2);
-  // const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-  //   dispatch(actions.removeCompleted());
-  // };
-
-  // const renderClearButton = () => {
-  //   if (completedCount > 0) {
-  //     return (
-  //       <button className="clear-completed" onClick={handleButtonClick}>
-  //         Clear completed
-  //       </button>
-  //     );
-  //   }
-  //   return null;
-  // };
   return (
     <Styled
-    // className={cx({
-    //   isDisabled: activeFiltersCount < 2 && filter.active,
-    // })}
+      className={cx({
+        // prevent last active btn from disabling
+        isDisabled: activeFiltersCount < 2 && currentFilter.active,
+      })}
     >
       {/* eslint-disable jsx-a11y/anchor-is-valid */}
       <a
         href="#"
-        onClick={() => dispatch(actions.toggleFilter(props.type))}
+        onClick={() => dispatch(actions.toggleFilter(type))}
         className={cx({
-          selected: props.filter === props.type,
-          // active: filter.active,
-          active: true,
+          // selected: props.filter === props.type,
+          active: currentFilter.active,
           button: true,
         })}
       >
-        {props.children}
+        {children}
       </a>
       {/* eslint-enable jsx-a11y/anchor-is-valid */}
     </Styled>
@@ -64,34 +47,3 @@ const FilterLink: React.FC<Props> = React.memo((props) => {
 });
 
 export default FilterLink;
-
-// export default class FilterLink extends React.Component<any, any> {
-//   render() {
-//     const { activeFiltersCount, filters } = this.props;
-
-//     // get data of current filterLink
-//     const filter = filters.filter((filter: any) => {
-//       return filter.name == this.props.type.toLowerCase();
-//     })[0];
-//     // console.log(filter);
-//     return (
-//       <li
-//         className={cx({
-//           isDisabled: activeFiltersCount < 2 && filter.active,
-//         })}
-//       >
-//         <a
-//           href="#"
-//           onClick={() => this.props.toggleFilter(this.props.type)}
-//           className={cx({
-//             selected: this.props.filter === this.props.type,
-//             active: filter.active,
-//             button: true,
-//           })}
-//         >
-//           {this.props.children}
-//         </a>
-//       </li>
-//     );
-//   }
-// }
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -7,29 +7,15 @@ interface Props {
 }
 
 const Header: React.FC<Props> = React.memo((props) => {
-  const someProps = {
-    // toggleFilter: props.toggleFilter,
-    // filter: props.filter,
-    // filters: props.filters,
-    // activeFiltersCount: props.activeFiltersCount,
-  };
   return (
     <header className="header">
       <div className="header-title">Карточки</div>
       <div className="buttons">
         <ul className="filters">
-          <FilterLink type="GOLD" {...someProps}>
-            Gold
-          </FilterLink>
-          <FilterLink type="SILVER" {...someProps}>
-            Silver
-          </FilterLink>
-          <FilterLink type="WOOD" {...someProps}>
-            Wood
-          </FilterLink>
-          <FilterLink type="STANDART" {...someProps}>
-            Standart
-          </FilterLink>
+          <FilterLink type="GOLD">Gold</FilterLink>
+          <FilterLink type="SILVER">Silver</FilterLink>
+          <FilterLink type="WOOD">Wood</FilterLink>
+          <FilterLink type="STANDART">Standart</FilterLink>
         </ul>
       </div>
     </header>
